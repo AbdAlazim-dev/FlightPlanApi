28c8b4a460409b24a7b3c691136aa21fdecd74ad
unknown <abdalazimattya.dev@gmail.com>
2024-01-29 14:01:13 +0300

    create the first end point on the controller

---
+++ FlightPlanApi/Controllers/FlightPlanController.cs
@@ -0,0 +1,26 @@
+﻿using FlightPlanApi.Data;
+using FlightPlanApi.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FlightPlanApi.Controllers;
+
+[ApiController]
+[Route("api/v1/flightplan")]
+public class FlightPlanController : ControllerBase
+{
+    private readonly IDatabaseAdapter _adapter;
+    public FlightPlanController(IDatabaseAdapter adapter) 
+    {
+        _adapter = adapter;
+    }
+    [HttpGet]
+    [ProducesResponseType(typeof(FlightPlan), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetAllFlightPlans()
+    {
+        var flightPlans = await _adapter.GetFlightPlans();
+
+        return Ok(flightPlans);
+    }
+}
--- FlightPlanApi/Program.cs
+++ FlightPlanApi/Program.cs
@@ -1,11 +1,20 @@
+using FlightPlanApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(setUpaction =>
+{
+    setUpaction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest));
+    setUpaction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status500InternalServerError));
+    setUpaction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status406NotAcceptable));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDatabaseAdapter, MongoDbDatabase>();
 
 var app = builder.Build();
 
