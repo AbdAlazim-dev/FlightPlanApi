1484ba14a49220b7f8385d0796cce02191a8d15c
unknown <abdalazimattya.dev@gmail.com>
2024-01-30 14:46:00 +0300

    add Xml comment support and document the api endpoints

--- FlightPlanApi/Controllers/FlightPlanController.cs
+++ FlightPlanApi/Controllers/FlightPlanController.cs
@@ -3,11 +3,12 @@ using FlightPlanApi.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace FlightPlanApi.Controllers;
 
 [ApiController]
-[Route("api/v1/flightplan")]
+[Route("api/v1/flightplans")]
 public class FlightPlanController : ControllerBase
 {
     private readonly IDatabaseAdapter _adapter;
@@ -18,6 +19,8 @@ public class FlightPlanController : ControllerBase
     [HttpGet]
     [ProducesResponseType(typeof(List<FlightPlan>), (int)HttpStatusCode.OK)]
     [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    //One way of documenting the api
+    [SwaggerResponse((int)StatusCodes.Status204NoContent, "No content found on the database")]
     public async Task<IActionResult> GetAllFlightPlans()
     {
         var flightPlans = await _adapter.GetFlightPlans();
@@ -28,10 +31,23 @@ public class FlightPlanController : ControllerBase
 
         return Ok(flightPlans);
     }
+    //the other way of documenting the api
+    /// <summary>
+    /// Get the flight plan by its id
+    /// </summary>
+    /// <param name="flightPlanId">the ID of the flight plan you want to get its info</param>
+    /// <response code="404">There is no flight plan with this id</response>
+    /// <response code="400">somthing wrong with the request you sent</response>
+    /// <response code="200">the flight plan info</response>
+    /// <response code="401">You are not authorized to use the endpoint</response>
+    /// <response code="406">The output format you entered in the "Accept header is not supported"</response>
+    /// <returns></returns>
     [HttpGet]
     [Route("GetFlightPlanById")]
     [ProducesResponseType(typeof(FlightPlan), (int)HttpStatusCode.OK)]
     [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    
+    
     public async Task<IActionResult> GetFlightPlanById(string flightPlanId)
     {
         var flightPlan = await _adapter.GetFlightPlan(flightPlanId);
@@ -42,8 +58,39 @@ public class FlightPlanController : ControllerBase
 
         return Ok(flightPlan);
     }
+    /// <summary>
+    /// Add flight plan to the system
+    /// </summary>
+    /// <remarks>
+    /// sample request
+    /// 
+    ///      POST : /api/v1/flightplans
+    ///      {
+    ///         "flight_plan_id": "f1d193ad0153491f9cf61cbe39c7db70",
+    ///         "altitude": 0,
+    ///         "airspeed": 0,
+    ///         "aircraft_identification" : "Hey You This is a test to create flight plan"
+    ///         "aircraft_type" :"string",
+    ///         "arrival_airport" : "string",
+    ///         "departing_airport": "string"
+    ///         "flight_type" : "string",
+    ///         "eparture_time": 2024-01-30T10:33:09.733+00:00
+    ///         "estimated_arrival_time" : 2024-01-30T10:33:09.733+00:00
+    ///         "route": "string",
+    ///         "remarks": "string",
+    ///         "fuel_hours": 0,
+    ///         "fuel_minutes": 0
+    ///         "number_onboard": 4,
+    ///         "numberOnBoard": 0
+    ///      }
+    /// </remarks>
+    /// <param name="flightPlan">the flight plan info you want to add to the system</param>
+    /// <response code="400">somthing wrong with the request you sent</response>
+    /// <response code="201">the flight plan has been added to the system</response>
+    /// <response code="401">You are not authorized to use the endpoint</response>
+    /// <response code="406">The output format you entered in the "Accept header is not supported"</response>
+    /// <returns></returns>
     [HttpPost]
-    [Route("route")]
     public async Task<IActionResult> CreateFlightPlan([FromBody] FlightPlan flightPlan)
     {
         var result = await _adapter.FileFlightPlan(flightPlan);
@@ -58,6 +105,37 @@ public class FlightPlanController : ControllerBase
                 return StatusCode(StatusCodes.Status500InternalServerError);
         }
     }
+    /// <summary>
+    /// edit flight plan by providing its id
+    /// add the flight plan id and provide new values throgh the request body
+    /// </summary>
+    /// <remarks>
+    /// sample request
+    /// 
+    ///     PUT : /api/v1/flightplans 
+    ///     {
+    ///         "flight_plan_id": "f1d193ad0153491f9cf61cbe39c7db70",
+    ///         "altitude": 0,
+    ///         "airspeed": 0,
+    ///         "aircraft_identification": "Hey You This is a test to update a field",
+    ///         "aircraft_type": "string",
+    ///         "arrival_airport": "string",
+    ///         "departing_airport": "string",
+    ///         "flight_type": "string",
+    ///         "departure_time": "2024-01-30T10:33:09.733+00:00",
+    ///         "estimated_arrival_time": "2024-01-30T10:33:09.733+00:00",
+    ///         "route": "string",
+    ///         "remarks": "string",
+    ///         "fuel_hours": 0,
+    ///         "fuel_minutes": 0,
+    ///         "number_onboard": 4,
+    ///         "numberOnBoard": 0
+    ///     }
+    /// </remarks>
+    /// <param name="flightPlanId">the flight plan ID you want to edit its info</param>
+    /// <response code="400">somthing wrong with the request you sent -usually the request body-</response>
+    /// <response code="200">the flight plan has been updated</response>
+    /// <response code="500">somthing went wrong on the server</response>
     [HttpPut]
     public async Task<IActionResult> UpdateFlightPlan(FlightPlan flightPlan)
     {
@@ -73,6 +151,16 @@ public class FlightPlanController : ControllerBase
                 return StatusCode(StatusCodes.Status500InternalServerError);
         }
     }
+    //the other way of documenting the api
+    /// <summary>
+    /// Delete the flight plan by its id
+    /// </summary>
+    /// <param name="flightPlanId">the ID of the flight plan you to delete</param>
+    /// <response code="404">There is no flight plan with this id</response>
+    /// <response code="400">somthing wrong with the request you sent</response>
+    /// <response code="200">the flight plan has been deleted</response>
+    /// <response code="401">You are not authorized to use the endpoint</response>
+    /// <response code="406">The output format you entered in the "Accept header is not supported"</response>
     [HttpDelete("{flightPlanId}")]
     public async Task<IActionResult> DeleteFlightPlan(string flightPlanId)
     {
@@ -84,6 +172,16 @@ public class FlightPlanController : ControllerBase
 
         return NotFound();
     }
+    //the other way of documenting the api
+    /// <summary>
+    /// Get the flight plan departure by its id
+    /// </summary>
+    /// <param name="flightPlanId">the ID of the flight plan you want to get its info</param>
+    /// <response code="404">There is no flight plan with this id</response>
+    /// <response code="400">somthing wrong with the request you sent</response>
+    /// <response code="200">the flight plan departure</response>
+    /// <response code="401">You are not authorized to use the endpoint</response>
+    /// <response code="406">The output format you entered in the "Accept header is not supported"</response>
     [HttpGet]
     [Route("airport/departure/{flightPlanId}")]
     public async Task<IActionResult> GetFlightPlanDepartureAirport(string flightPlanId)
@@ -97,6 +195,16 @@ public class FlightPlanController : ControllerBase
 
         return Ok(flightPlan.DepartureAirport);
     }
+    //the other way of documenting the api
+    /// <summary>
+    /// Get the flight plan route by its id
+    /// </summary>
+    /// <param name="flightPlanId">the ID of the flight plan you want to get its info</param>
+    /// <response code="404">There is no flight plan with this id</response>
+    /// <response code="400">somthing wrong with the request you sent</response>
+    /// <response code="200">the flight plan route</response>
+    /// <response code="401">You are not authorized to use the endpoint</response>
+    /// <response code="406">The output format you entered in the "Accept header is not supported"</response>
     [HttpGet]
     [Route("route/{flightPlanId}")]
     public async Task<IActionResult> GetFlightPlanRoute(string flightPlanId)
@@ -110,6 +218,16 @@ public class FlightPlanController : ControllerBase
 
         return Ok(flightPlan.Route);
     }
+    //the other way of documenting the api
+    /// <summary>
+    /// Get the flight plan enroute time by its id
+    /// </summary>
+    /// <param name="flightPlanId">the ID of the flight plan you want to get its info</param>
+    /// <response code="404">There is no flight plan with this id</response>
+    /// <response code="400">somthing wrong with the request you sent</response>
+    /// <response code="200">the flight plan enroute</response>
+    /// <response code="401">You are not authorized to use the endpoint</response>
+    /// <response code="406">The output format you entered in the "Accept header is not supported"</response>
     [HttpGet]
     [Route("Time/enroute/")]
     public async Task<IActionResult> GetFlightPlanTimeInRoute(string flightPlanId)
--- FlightPlanApi/Program.cs
+++ FlightPlanApi/Program.cs
@@ -1,7 +1,11 @@
+using Amazon.Runtime.Internal.Transform;
 using FlightPlanApi.Auth;
 using FlightPlanApi.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +17,50 @@ builder.Services.AddControllers(setUpaction =>
     setUpaction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest));
     setUpaction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status500InternalServerError));
     setUpaction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status406NotAcceptable));
+    setUpaction.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status401Unauthorized));
+    setUpaction.Filters.Add(new AuthorizeFilter());
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setUpAction =>
+{
+    setUpAction.SwaggerDoc("flightplan", new()
+    {
+        Title = "Flight Plan Api",
+        Version = "v3",
+        Description = "A description of the api that provides a flight plans info and route to update, create, and delete them"
+    });
+
+    var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlFullPathFile = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+
+    setUpAction.IncludeXmlComments(xmlFullPathFile);
+
+    //Authentication setUp
+    setUpAction.AddSecurityDefinition("BasicAuthentication", new()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "basic",
+        Description = "basic authentication to protect the api -not the best approach-",
+        In = ParameterLocation.Header
+    });
+
+    setUpAction.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "BasicAuthentication"
+                }
+            },
+            new List<string>()
+        }
+    });
+
+});
 builder.Services.AddScoped<IDatabaseAdapter, MongoDbDatabase>();
 builder.Services.AddAuthentication("BasicAuthentication").AddScheme
     <AuthenticationSchemeOptions, BasicAuthenticationHandler>
@@ -28,7 +72,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/flightplan/swagger.json", "Flight Plan API");
+    });
 }
 
 app.UseHttpsRedirection();
